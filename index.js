329b6a37a7b46176d3b67f190fd491e819fa381b
illiakalinin <illinoiz4305@gmail.com>
2023-02-26 17:24:41 +0200

    Add index.html

--- index.js
+++ index.js
@@ -1,56 +1,23 @@
-const os = require('os');
-const fs = require('fs');
-// console.log('os.platform() :>> ', os.platform());
-// console.log('os.cpus() :>> ', os.cpus());
+const { readFile } = require('fs/promises');
+const http = require('http');
 
-fs.readFile('./math.js', { encoding: 'utf-8' }, (err, data) => {
-  if (err) {
-    // console.log('err :>> ', err);
-  } else {
-    // console.log('data :>> ', data);
-  }
-});
+const httpServer = http.createServer((request, response) => {
+  console.log('url :>> ', request.url);
+  console.log('method :>> ', request.method);
 
-const fileText = fs.readFileSync('./math.js', { encoding: 'utf-8' });
-// console.log('fileText :>> ', fileText);
-
-fs.readdir('.', (err, data) => {
-  if (err) {
-    // console.log('err', err);
-  } else {
-    data
-      .filter(f => /^.*\.js$/.test(f))
-      .forEach(f =>
-        fs.readFile(f, { encoding: 'utf-8' }, (err, data) => {
-          if (err) {
-            // console.log('err :>> ', err);
-          } else {
-            // console.log('data :>> ', data);
-          }
-        })
-      );
-  }
-});
-
-try {
-  const files = fs.readdirSync('.');
-  // console.log('Files in directory:');
-  files
-    .filter(f => /^.*\.js$/.test(f))
-    .forEach(f => {
-      try {
-        const data = fs.readFileSync(f, { encoding: 'utf-8' });
-        // console.log('Data from file:', f);
-        // console.log(data);
-      } catch (err) {
-        // console.log('Error reading file:', f, err);
-      }
+  readFile('./index.html', { encoding: 'utf-8' })
+    .then(data => {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.end(data);
+    })
+    .catch(err => {
+      response.statusCode = 500;
+      response.setHeader('Content-Type', 'text/html');
+      response.end('File was not read');
     });
-} catch (err) {
-  // console.log('Error reading directory:', err);
-}
+});
 
-console.log('__filename :>> ', __filename);
-console.log('__dirname :>> ', __dirname);
-
-console.log('process :>> ', process.env);
+httpServer.listen(5000, 'localhost', () => {
+  console.log(`Server is listening port 5000, host "localhost"`);
+});
